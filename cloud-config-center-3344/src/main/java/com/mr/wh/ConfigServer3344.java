612ae694714103be59c84083c87e58eef1aa4d49
忘皓 <1421485150@qq.com>
2020-08-08 10:28:01 +0800

    提交configsever 的主启动

---
+++ cloud-config-center-3344/src/main/java/com/mr/wh/ConfigServer3344.java
@@ -0,0 +1,11 @@
+package com.mr.wh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServer3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer3344.class,args);
+    }
+}
