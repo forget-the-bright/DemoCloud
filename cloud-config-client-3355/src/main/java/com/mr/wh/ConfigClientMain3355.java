18ac4bc19a21fc4c65b9a5801e2eb091e9f5fd05
忘皓 <1421485150@qq.com>
2020-08-08 11:30:49 +0800

    提交config client module

--- cloud-config-client-3355/src/main/java/com/mr/wh/ConfigClientMain3355.java
+++ cloud-config-client-3355/src/main/java/com/mr/wh/ConfigClientMain3355.java
@@ -1,13 +1,14 @@
 package com.mr.wh;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigServer3344 {
+@EnableEurekaClient
+public class ConfigClientMain3355 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServer3344.class,args);
+        SpringApplication.run(ConfigClientMain3355.class,args);
     }
 }
