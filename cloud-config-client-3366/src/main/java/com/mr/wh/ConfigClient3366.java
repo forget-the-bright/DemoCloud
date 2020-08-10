8520ac11f3235cd3cac9c80de6222b541ec8b959
15128022404 <1421485150@qq.com>
2020-08-10 20:12:40 +0800

    提交新的config client 3366

---
+++ cloud-config-client-3366/src/main/java/com/mr/wh/ConfigClient3366.java
@@ -0,0 +1,12 @@
+package com.mr.wh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClient3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient3366.class,args);
+    }
+}
