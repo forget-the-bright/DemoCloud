2125beb66165817bbdb050d92555081832158802
15128022404 <1421485150@qq.com>
2020-08-11 11:23:06 +0800

    修改3366主启动名，使其符合规范

--- cloud-config-client-3366/src/main/java/com/mr/wh/ConfigClientMain3366.java
+++ cloud-config-client-3366/src/main/java/com/mr/wh/ConfigClientMain3366.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConfigClient3366 {
+public class ConfigClientMain3366 {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigClient3366.class,args);
+        SpringApplication.run(ConfigClientMain3366.class,args);
     }
 }
