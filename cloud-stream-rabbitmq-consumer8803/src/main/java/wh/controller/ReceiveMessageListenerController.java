72c4e35d9c30e0685bd67e976f027ab9625cbd1b
15128022404 <1421485150@qq.com>
2020-08-12 11:51:21 +0800

    Stream消息驱动之消费者模块，添加分组 atmingrui 测试消息持久化

--- cloud-stream-rabbitmq-consumer8803/src/main/java/wh/controller/ReceiveMessageListenerController.java
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/wh/controller/ReceiveMessageListenerController.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
-
 @Component
 @EnableBinding(Sink.class)
 public class ReceiveMessageListenerController {
