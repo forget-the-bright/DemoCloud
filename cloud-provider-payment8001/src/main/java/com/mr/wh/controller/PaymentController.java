8841402971e43eecbfe1aa7460d6939f21f8fda4
15128022404 <1421485150@qq.com>
2020-08-12 12:00:45 +0800

    配置分布式消息链路，zipkin 8001,Sleuth

--- cloud-provider-payment8001/src/main/java/com/mr/wh/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/mr/wh/controller/PaymentController.java
@@ -76,4 +76,8 @@ public class PaymentController {
         }
         return serverPort+"";
     }
+    @GetMapping("/payment/zipkin")
+    public String paymentZipkin(){
+        return "hi i am paymentZipkin server fall back,welcome to mingrui,d=====(￣▽￣*)b";
+    }
 }
