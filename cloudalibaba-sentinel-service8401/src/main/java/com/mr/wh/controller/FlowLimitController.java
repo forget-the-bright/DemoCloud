0de5a6dde54678dfd9186fc4f0bbaa1d6409b1e6
15128022404 <1421485150@qq.com>
2020-08-20 22:09:55 +0800

    提交sentinel 热点key限流 @sentinelResource 注解使用 只对sentinel限流规则生效，不对系统异常生效

--- cloudalibaba-sentinel-service8401/src/main/java/com/mr/wh/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/mr/wh/controller/FlowLimitController.java
@@ -1,7 +1,12 @@
 package com.mr.wh.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Description:
@@ -14,10 +19,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class FlowLimitController {
    @GetMapping("/testA")
    public String testA(){
+       System.out.println("testA");
        return  "--------testA";
    }
    @GetMapping("/testB")
    public String testB(){
+       System.out.println("testB");
        return  "--------testB";
+   }
+   @GetMapping("/testC")
+   public String testC(){
+       try {
+           TimeUnit.SECONDS.sleep(1);
+       } catch (InterruptedException e) {
+           e.printStackTrace();
+       }
+       System.out.println("testC");
+       return  "--------testC";
+   }
+   @GetMapping("/testD")
+   @SentinelResource(value = "hotkey_TestD",blockHandler = "hotKey_Method")
+   public String testD(@RequestParam(value = "p1" ,required = false) String p1,
+                       @RequestParam(value = "p2" ,required = false) String p2){
+       System.out.println("testD");
+       return  "--------testD";
+   }
+   public String hotKey_Method(String p1, String p2, BlockException exc){
+       return "d=====(￣▽￣*)b没错，你出错了！！！";
    }
 }
